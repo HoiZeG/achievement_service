0efb254bc4bb48244e85f10999dbb0f5aa305d53
Maksim Kononov <07maks7@mail.ru>
2024-10-25 11:37:16 +0300

    start solution BJS2-27479(Получение достижения "Красавчик") sub

--- src/main/java/faang/school/achievement/config/redis/RedisConfig.java
+++ src/main/java/faang/school/achievement/config/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package faang.school.achievement.config.redis;
 
 import faang.school.achievement.listener.LikeEventListener;
+import faang.school.achievement.listener.ProfileEventListener;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,20 +24,31 @@ public class RedisConfig {
         return new MessageListenerAdapter(likeEventListener);
     }
 
+    @Bean
+    MessageListenerAdapter profileEventAdapter(ProfileEventListener profileEventListener) {
+        return new MessageListenerAdapter(profileEventListener);
+    }
+
     @Bean(value = "likeChannelTopic")
     ChannelTopic likeChannelTopic(@Value("${spring.data.redis.channel.like-channel.name}") String likeChannelName) {
         return new ChannelTopic(likeChannelName);
     }
 
+    @Bean(value = "profileChannelTopic")
+    ChannelTopic profileChannelTopic(@Value("${spring.data.redis.channel.profile-channel.name}") String profilePicChannelName) {
+        return new ChannelTopic(profilePicChannelName);
+    }
+
     @Bean
     RedisMessageListenerContainer redisMessageListenerContainer(
             LikeEventListener likeEventListener,
-            @Qualifier("likeChannelTopic") ChannelTopic likeChannelTopic
-    ) {
+            ProfileEventListener profileEventListener,
+            @Qualifier("likeChannelTopic") ChannelTopic likeChannelTopic,
+            @Qualifier("profileChannelTopic") ChannelTopic profileChannelTopic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(jedisConnectionFactory());
         container.addMessageListener(likeEventListener, likeChannelTopic);
-
+        container.addMessageListener(profileEventListener, profileChannelTopic);
         return container;
     }
 }
